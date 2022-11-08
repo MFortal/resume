5e91520f73e3dd9bbcbb036ab90e902ec466a6aa
Polyakova Maria <polyakovamy@yandex.ru>
2022-11-08 15:18:18 +0300

    Адаптация шапки

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,12 @@
+import "../scss/reset.scss";
 import "../scss/main.scss";
 import "../scss/media.scss";
+
+const nav = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+
+navBtn.onclick = () => {
+  nav.classList.toggle("open")
+    ? navBtn.classList.add("close")
+    : navBtn.classList.remove("close");
+};
